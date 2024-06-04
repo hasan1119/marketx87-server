89ab1496aa0f095a1b37818c0ee3df84be515f4a
hasan1119 <rokibul.devs@gmail.com>
2024-06-04 12:09:51 +0600

    trust proxy added

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const { readdirSync } = require("fs");
 const path = require("path");
 // const fileUpload = require("express-fileupload");
 const bodyParser = require("body-parser");
-require('dotenv').config()
-
-
+require("dotenv").config();
 
 //Security Middleware Import
 const rateLimit = require("express-rate-limit");
@@ -16,8 +14,7 @@ const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const hpp = require("hpp");
 const morgan = require("morgan");
-
-
+app.set("trust proxy", true);
 
 // express app initialization
 const app = express();
@@ -28,11 +25,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRETE));
 // app.use(fileUpload());
 
-
 // Serve static files from the 'public' directory
 app.use(express.static("public"));
-
-
 
 // Security middleware initialization
 app.use(cors());
@@ -41,23 +35,20 @@ app.use(mongoSanitize());
 app.use(hpp());
 app.use(morgan("dev"));
 
-
 //Request Rate Limiting
 const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000, // 1 minutes
-    max: 10000, // limit each IP to 10000 requests per windowMs (change before the production)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 10000, // limit each IP to 10000 requests per windowMs (change before the production)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
 app.use(limiter);
 
-
 // Routing middleware initialization
 readdirSync("./src/routes").map((r) =>
-    app.use(`/api/v1`, require(`./src/routes/${r}`))
+  app.use(`/api/v1`, require(`./src/routes/${r}`))
 );
 
 // module exports
 module.exports = app;
-

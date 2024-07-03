eb90d1d68d7a715e0d5224d25156eeda03a26cba
hasan1119 <rokibul.devs@gmail.com>
2024-07-03 21:24:04 +0600

    OK

--- src/utils/fileUpload.config.js
+++ src/utils/fileUpload.config.js
@@ -1,14 +1,13 @@
-const path = require('path');
+const path = require("path");
 
 const imagConfig = {
-
-    profile: {
-        DIR: path.join(__dirname + './../../public/files/profile'),
-        SIZE: 10000000,
-        MINE: ['image/jpg', 'image/jpeg', 'image/png', 'image/svg+xml'],
-    }
+  profile: {
+    DIR: path.join(__dirname + "./../../public/files/profile"),
+    SIZE: 10000000,
+    MINE: ["image/jpg", "image/jpeg", "image/png", "image/svg+xml"],
+  },
 };
 
 module.exports = {
-    imagConfig,
+  imagConfig,
 };
